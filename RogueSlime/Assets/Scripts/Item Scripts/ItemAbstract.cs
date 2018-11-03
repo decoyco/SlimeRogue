6626ecfc5ccd08df22437f80e419453de09e493a
Jason Wu <wujason0@gmail.com>
2018-11-03 11:54:10 -0700

    added item move, needs testing

--- RogueSlime/Assets/Scripts/Item Scripts/ItemAbstract.cs
+++ RogueSlime/Assets/Scripts/Item Scripts/ItemAbstract.cs
@@ -4,20 +4,45 @@ using UnityEngine;
 
 public abstract class ItemAbstract : MonoBehaviour
 {
+    public float moveSpeed = 1;
+    private bool isMoveToTriggered = false;
+    private Vector3 locationToMoveTo;
+
     public abstract void onPickupAction(Entity other);
 
     public void destroy()
     {
         Destroy(gameObject);
     }
-    
+
     public void moveTo(GameObject other)
     {
-        moveTo(new Vector2(other.transform.position.x, other.transform.position.y));
+        //NOTE MAY NEED TO UPDATE THIS INSTEAD OF JUST GETTING ONCE
+        //OR MAYBE CONTINUOUS CALLS WHEN IN RANGE ARE OK
+        moveTo(other.transform.position);
     }
 
-    public void moveTo(Vector2 v)
+    public void moveTo(Vector3 v)
     {
-        
+        locationToMoveTo = v;
+        isMoveToTriggered = true;
     }
+
+    public void stopMoveTo()
+    {
+        isMoveToTriggered = false;
+    }
+
+    public void Update()
+    {
+        if (isMoveToTriggered)
+        {
+            Vector3 relative_position = locationToMoveTo - transform.position;
+            relative_position.Normalize();
+
+            transform.position += relative_position * Time.deltaTime * .1f * moveSpeed;
+        }
+
+    }
+
 }
