390d07aa4aa80caa4c8593aaed60c4870f62ae87
Jason Wu <wujason0@gmail.com>
2018-11-03 11:33:41 -0700

    initial setup of objects/functions

---
+++ RogueSlime/Assets/Scripts/AttractBubble.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttractBubble : MonoBehaviour {
+
+    public bool isEnabled;
+
+	// Use this for initialization
+	void Start () {
+        isEnabled = false;
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //if the other is a Drop
+        hitDrop(other);
+    }
+
+    public void hitDrop(Collider2D other)
+    {
+        ItemAbstract item = other.gameObject.GetComponent<ItemAbstract>();
+        if (item != null)
+        {
+            item.moveTo(gameObject);
+        }
+    }
+
+    /*
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    */
+
+}
--- RogueSlime/Assets/Scripts/Item Scripts/ItemAbstract.cs
+++ RogueSlime/Assets/Scripts/Item Scripts/ItemAbstract.cs
@@ -5,8 +5,19 @@ using UnityEngine;
 public abstract class ItemAbstract : MonoBehaviour
 {
     public abstract void onPickupAction(Entity other);
+
     public void destroy()
     {
         Destroy(gameObject);
+    }
+    
+    public void moveTo(GameObject other)
+    {
+        moveTo(new Vector2(other.transform.position.x, other.transform.position.y));
+    }
+
+    public void moveTo(Vector2 v)
+    {
+        
     }
 }
